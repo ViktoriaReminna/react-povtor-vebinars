24260ec471f57871ce39342dda3d3886009f059c
ViktoriaReminna <121427776+ViktoriaReminna@users.noreply.github.com>
2023-08-18 22:29:23 +0300

    modul2 lesson1

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,28 @@
 import { QuizList } from './QuizList/QuizList';
-import quizItems from '../data.json';
+import initialQuizItems from '../data.json';
 import { SearchBar } from './SearchBar/SearchBar';
-import { GlobalStyle } from './GlobalStyle';
 import { Layout } from './Layout';
-import { HiAcademicCap, HiAdjustments, HiArchive } from 'react-icons/hi';
-import { IconButton } from './IconButton/IconButton';
+import { Component } from 'react';
 
-export const App = () => {
-  return (
-    <Layout>
-      <SearchBar />
-      <QuizList items={quizItems} />
+export class App extends Component {
+  state = {
+    quizItems: initialQuizItems,
+  };
 
-      <IconButton variant="primary" size="sm">
-        <HiAcademicCap />
-      </IconButton>
-      <IconButton variant="secondary" size="md">
-        <HiArchive />
-      </IconButton>
-      <IconButton variant="secondary" size="lg">
-        <HiAdjustments />
-      </IconButton>
+  handleDelete = quizId => {
+    this.setState(prevState => {
+      return {
+        quizItems: prevState.quizItems.filter(quiz => quiz.id !== quizId),
+      };
+    });
+  };
 
-      <GlobalStyle />
-    </Layout>
-  );
-};
+  render() {
+    return (
+      <Layout>
+        <SearchBar />
+        <QuizList items={this.state.quizItems} onDelete={this.handleDelete} />
+      </Layout>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'components/GlobalStyle';
 import { App } from 'components/App';
-import './index.css';
+// import { StateExample } from 'components/StateExample/StateExample';
+
+const theme = {
+  colors: {
+    black: '#212121',
+    white: '#fff',
+    success: 'green',
+    warning: 'orange',
+    error: 'red',
+  },
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+      {/* <StateExample /> */}
+      <GlobalStyle />
+    </ThemeProvider>
   </React.StrictMode>
 );
